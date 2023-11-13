9858a7ecde68b56f250251035174416485a561a5
MARUF3470 <smmaruf25@gmail.com>
2023-11-14 01:19:39 +0600

    change fetching way and used vercel postgres storage

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,10 +1,10 @@
-import { NextResponse } from "next/server";
-import vine, {errors} from '@vinejs/vine'
+import { NextRequest, NextResponse } from "next/server";
+import vine, { errors } from '@vinejs/vine'
 import { loginSchema } from "@/validation/registrationSchema";
 import { CustomErrorReporter } from "@/validation/customErrorReporter";
-import  { compareSync } from 'bcryptjs'
+import { compareSync } from 'bcryptjs'
 import prisma from "@/DB/db.config";
-export const POST = async (request: NextResponse) => {
+export const POST = async (request: NextRequest) => {
     try {
         const data = await request.json()
         vine.errorReporter = () => new CustomErrorReporter()
@@ -17,21 +17,21 @@ export const POST = async (request: NextResponse) => {
         })
 
         if (!findUser) {
-            return NextResponse.json({ status: 400, error:{email: 'No account found with this email' }})
+            return NextResponse.json({ status: 400, error: { email: 'No account found with this email' } })
         }
         // * check password
-        
+
         const checkPassword = compareSync(payload.password, findUser.password!)
         if (checkPassword) {
-            return NextResponse.json({status: 200, message:"User login successfully"})  
+            return NextResponse.json({ status: 200, message: "User login successfully" })
         }
-        return NextResponse.json({status: 400, error:{password:"Invalid Password"}})
+        return NextResponse.json({ status: 400, error: { password: "Invalid Password" } })
 
     } catch (error) {
         if (error instanceof errors.E_VALIDATION_ERROR) {
-           
-          return  NextResponse.json({status: 400, error: error.messages})
-          }
+
+            return NextResponse.json({ status: 400, error: error.messages })
+        }
     }
-    
+
 }
